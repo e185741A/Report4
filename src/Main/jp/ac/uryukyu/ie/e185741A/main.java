033454e022bdcb5489f1b22797b31b21776a3f2b
Atsuto Goya <e185741@ie.u-ryukyu.ac.jp>
2018-11-26 15:56:51 +0900

    Report4_1

--- src/Main/jp/ac/uryukyu/ie/e185741A/main.java
+++ src/Main/jp/ac/uryukyu/ie/e185741A/main.java
@@ -2,7 +2,14 @@ package jp.ac.uryukyu.ie.e185741A;
 
 public class main {
     public static void main(String[] args) {
-        String[] str = null;
-        System.out.println(str.length);
-    }
+        System.out.println("開始します");
+        try{
+            String[] str = null;
+            System.out.println(str.length);
+        }
+        catch(NullPointerException e){
+            System.out.println("配列の範囲を超えています");
+        }
+        System.out.println("終わりました");
+        }
 }
