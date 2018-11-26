8bf9c1156dc32a6ca052676c6761c207c6cf59d5
Atsuto Goya <e185741@ie.u-ryukyu.ac.jp>
2018-11-26 14:59:24 +0900

    Report4

---
+++ src/Main/jp/ac/uryukyu/ie/e185741/main.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e185741;
+
+public class main {
+    public static void main(String[] args) {
+        String[] str = null;
+        System.out.println(str.length);
+    }
+}
